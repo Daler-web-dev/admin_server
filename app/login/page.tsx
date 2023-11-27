7daf7a11e822f7f5ada126489f20ba726a70949b
Daler <dsarifkulo021@gmail.com>
2023-11-27 23:32:45 +0500

    fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,13 +4,13 @@ export default function Login() {
 	return (
 		<div className="w-full h-screen flex justify-center items-center bg-[#F3F6FE]" >
             <div className="w-[400px]" >
-				<h1 className="font-normal mb-3 text-2xl">Вход в аккаунт</h1>
+				<h1 className="font-normal mb-3 text-2xl">Вход в аккаунт loshara</h1>
 				<form className="flex flex-col"  >
 					<label htmlFor="login" className="text-[9px] uppercase mb-2"  >ЛОГИН</label>
-					<input className="p-3 rounded-md" type="text" placeholder="Логин" name="login" />
+					<input id="login" className="p-3 rounded-md" type="text" placeholder="Логин" name="login" />
 					<br />
 					<label htmlFor="password" className="text-[9px] uppercase mb-2" >ПАРОЛЬ</label>
-					<input className="p-3 rounded-md" type="text" placeholder="Пароль" name="password" />
+					<input id="password" className="p-3 rounded-md" type="text" placeholder="Пароль" name="password" />
 					<br />
 					<button className="bg-[#007aff] text-white p-3 rounded-md" >Войти</button>
 				</form>
