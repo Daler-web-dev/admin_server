81e14652579472efa9ace4be44141e7b24479074
Daler <dsarifkulo021@gmail.com>
2023-11-28 23:57:48 +0500

    fixed quit

--- components/TableItem.tsx
+++ components/TableItem.tsx
@@ -11,24 +11,13 @@ const TableItem: React.FC<TableItemProps> = ({idx}) => {
 			<td className="w-4 p-4">
 				<div className="flex items-center">
 					{idx + 1}
-					{/* <input
-						id="checkbox-table-search-1"
-						type="checkbox"
-						className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-					/>
-					<label
-						htmlFor="checkbox-table-search-1"
-						className="sr-only"
-					>
-						checkbox
-					</label> */}
 				</div>
 			</td>
 			<th
 				scope="row"
 				className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
 			>
-				Apple MacBook Pro 17"
+				Apple MacBook Pro 17
 			</th>
 			<td className="px-6 py-4">Silver</td>
 			<td className="px-6 py-4">Laptop</td>
