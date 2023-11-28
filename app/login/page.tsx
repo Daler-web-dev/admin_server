09a485265c2bf7ec348535c171c5763603a3895f
Daler <dsarifkulo021@gmail.com>
2023-11-28 22:57:29 +0500

    login was created

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,14 @@
+import Login from "@/components/containers/Login";
 import Image from "next/image";
 
-export default function Login() {
+export default function LoginPage() {
 	return (
-		<div className="w-full h-screen flex justify-center items-center bg-[#F3F6FE]" >
-            <div className="w-[400px]" >
-				<h1 className="font-normal mb-3 text-2xl">Вход в аккаунт loshara</h1>
-				<form className="flex flex-col"  >
-					<label htmlFor="login" className="text-[9px] uppercase mb-2"  >ЛОГИН</label>
-					<input id="login" className="p-3 rounded-md" type="text" placeholder="Логин" name="login" />
-					<br />
-					<label htmlFor="password" className="text-[9px] uppercase mb-2" >ПАРОЛЬ</label>
-					<input id="password" className="p-3 rounded-md" type="text" placeholder="Пароль" name="password" />
-					<br />
-					<button className="bg-[#007aff] text-white p-3 rounded-md" >Войти</button>
-				</form>
-            </div>
+		<div className="w-full h-screen flex justify-center items-center bg-[#F3F6FE]">
+			<div className="w-[400px]">
+				<h1 className="font-normal mb-3 text-2xl">Вход в аккаунт</h1>
+				<Login/>
+			</div>
 		</div>
 	);
 }
+
---
+++ components/containers/Login.tsx
@@ -0,0 +1,87 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useRouter } from 'next/navigation'
+
+type Inputs = {
+	login: string;
+	password: string;
+};
+
+const Login = () => {
+	const [loading, setLoading] = useState(false);
+    const router = useRouter()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>();
+
+
+	const onSubmit: SubmitHandler<Inputs> = async (data) => {
+		setLoading(true);
+		try {
+			const res = await axios.post(
+				process.env.NEXT_PUBLIC_API + "/authentication",
+				{ strategy: "local", ...data }
+			);
+
+			if (res.status === 201 || res.status === 200) {
+                const {data} = res
+                const token = {
+                    token: data.accessToken,
+                    exp: data.authentication.payload.exp
+                }
+
+                localStorage.setItem('token', JSON.stringify(token))
+				setLoading(false);
+                router.push('/home')
+			}
+		} catch (e) {
+			console.log({ e });
+			setLoading(false);
+		}
+	};
+
+	return (
+		<form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+			<label htmlFor="login" className="text-[9px] uppercase mb-2">
+				ЛОГИН
+			</label>
+			<input
+				id="login"
+				className="p-3 rounded-md"
+				type="text"
+				placeholder="Логин"
+				{...register("login", { required: true })}
+			/>
+			<span className="text-red-500">
+				{errors.login && "Заполните это поле"}
+			</span>
+			<br />
+			<label htmlFor="password" className="text-[9px] uppercase mb-2">
+				ПАРОЛЬ
+			</label>
+			<input
+				id="password"
+				className="p-3 rounded-md"
+				type="text"
+				placeholder="Пароль"
+				{...register("password", { required: true })}
+			/>
+			<span className="text-red-500">
+				{errors.password && "Заполните это поле"}
+			</span>
+			<br />
+			<button
+				type="submit"
+				className="bg-[#007aff] text-white p-3 rounded-md"
+			>
+				{loading ? "Loading..." : "Войти"}
+			</button>
+		</form>
+	);
+};
+
+export default Login;
