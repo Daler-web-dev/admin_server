1f3077f5c62bfd263d0d77eb08b5ff5249744778
Daler <dsarifkulo021@gmail.com>
2023-12-12 14:31:25 +0500

    fixed

--- app/(me)/home/page.tsx
+++ app/(me)/home/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
 		<div>
 			<h1 className="mb-4 font-normal text-xl" >Welcome</h1>
 			<span>Video guide</span>
-			<video className="w-full aspect-video rounded-md" width="100%" height="100%" src="https://storage.cloud.google.com/amadea/2023-12-12%2012-20-59.mp4" controls ></video>
+			<video className="mt- w-full aspect-video rounded-md" width="100%" height="100%" src="https://storage.cloud.google.com/amadea/2023-12-12%2012-20-59.mp4" controls ></video>
 		</div>
 	);
 }
