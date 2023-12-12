32f58c8315ad214ebc957278ed1114d8ab0801d7
Daler <dsarifkulo021@gmail.com>
2023-12-12 13:57:11 +0500

    video integrated

--- app/(me)/home/page.tsx
+++ app/(me)/home/page.tsx
@@ -3,8 +3,9 @@ export default async function Home() {
 	return (
 		<div>
 			<h1 className="mb-4 font-normal text-xl" >Welcome</h1>
-			<div className="w-full bg-[#daebff] rounded-md h-[500px] flex justify-center items-center">
-				<h2 className="text-[3xl] font-bold" >Video guide soon</h2>
+			<span>Video guide</span>
+			<div className="mt-4 w-full bg-[rgb(218,235,255)] h-[500px] flex justify-center items-center rounded-md overflow-hidden ">
+				<video width="100%" height="100%" src="https://storage.cloud.google.com/amadea/2023-12-12%2012-20-59.mp4" controls ></video>
 			</div>
 		</div>
 	);
